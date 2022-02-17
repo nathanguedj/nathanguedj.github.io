4329213181faa0bc2a24cfe2987ee9b128c7f46f
nathanguedj <99882398+nathanguedj@users.noreply.github.com>
2022-02-17 15:33:58 +0100

    Delete scripts.js

--- scripts.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".levelbar>div").forEach(a => {
-    let alreadySet = false
-    window.addEventListener("scroll", () => {
-        if(window.scrollY + window.innerHeight > a.offsetTop + window.innerHeight * 1.1 && !alreadySet){
-            alreadySet = true
-            a.style.width = a.getAttribute("length") + "%"
-        }
-    })
-})
